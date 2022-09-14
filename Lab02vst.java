06d21018319639a4c54d33c51457457792c972d0
14276 <analiese.drupka@vtsd.com>
2022-09-14 09:25:39 -0400

    t

--- Lab02vst.java
+++ Lab02vst.java
@@ -9,6 +9,14 @@
   public class Lab02vst {
 
     public static void main(String args[]){
-
-    }
+      System.out.println("Lab 02, 100 Point Version");
+      System.out.println();
+      System.out.println();
+      System.out.println("Measure today's knowledge by yesterday's confusion. ");
+      System.out.println();
+      System.out.println("Bewilderment + Exposure = Obvious");
+      System.out.println();
+      System.out.println("# ##### #####     ##### ##### # #   # ##### #####");
+      System.out.println("# #   # #   #     #   # #   # # #")
+  }
   }
